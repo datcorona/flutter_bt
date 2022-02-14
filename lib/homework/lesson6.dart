d346180f18f1ed67db21162487fd76c824810c13
datcorona <dat.corona97@gmail.com>
2022-02-14 15:59:12 +0700

    update UI lesson 6

--- lib/homework/lesson6.dart
+++ lib/homework/lesson6.dart
@@ -49,37 +49,36 @@ class AndroidWidget extends StatelessWidget {
       margin: const EdgeInsets.all(16),
       child: Row(
         children: [
-          Flexible(
+          Expanded(
+              flex: 1,
               child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                "Lập trình Android",
-                style: TextStyle(
-                    fontSize: 26,
-                    color: Colors.black,
-                    fontWeight: FontWeight.w400),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Java + Kotlin",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.black,
-                ),
-              )
-            ],
-          )),
-          Flexible(
-              child: Container(
-            margin: const EdgeInsets.only(left: 16),
-            child: Image.network(
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlWvB7JEzj-2BjXDBtOl_WHFE4zRsniCyjIQ&usqp=CAU'),
-          )),
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    "Lập trình Android",
+                    style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w400),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Java + Kotlin",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  )
+                ],
+              )),
+          Expanded(
+              flex: 1,
+              child: Image.network(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlWvB7JEzj-2BjXDBtOl_WHFE4zRsniCyjIQ&usqp=CAU')),
         ],
       ),
     );
@@ -95,38 +94,37 @@ class JavaCoreWidget extends StatelessWidget {
       margin: const EdgeInsets.all(16),
       child: Row(
         children: [
-          Flexible(
+          Expanded(
+              flex: 1,
               child: Image.network(
-                  'https://unica.vn/media/imagesck/1624420787_Java-Core-la-gi.jpg?v=1624420787')),
-          Flexible(
-              child: Container(
-            margin: const EdgeInsets.only(left: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text(
-                  "Lập trình Java cơ bản",
-                  style: TextStyle(
-                      fontSize: 26,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w400),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "Cho người mới bắt đầu",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black,
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBy7lAhCz9Jeq2b0EPQ6yaI2Za4WUPBgfnVtaRptwu306CLHUzHP81aLe_hHvSvZwPAr0&usqp=CAU')),
+          Expanded(
+              flex: 1,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    "Lập trình Java cơ bản",
+                    style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w400),
+                    textAlign: TextAlign.center,
                   ),
-                )
-              ],
-            ),
-          ))
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Cho người mới bắt đầu",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  )
+                ],
+              ))
         ],
       ),
     );
@@ -142,39 +140,37 @@ class FlutterWidget extends StatelessWidget {
       margin: const EdgeInsets.all(16),
       child: Row(
         children: [
-          Flexible(
-              child: Image.network(
-                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStE80StFXxu1ku5vp52LpR3DLx4OXUmby_7A&usqp=CAU'),
+          Expanded(
+            flex: 1,
+            child: Image.network(
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStE80StFXxu1ku5vp52LpR3DLx4OXUmby_7A&usqp=CAU'),
           ),
-          Flexible(
-              child: Container(
-            margin: const EdgeInsets.only(left: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Text(
-                  "Lập trình Flutter",
-                  style: TextStyle(
-                      fontSize: 26,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w400),
-                textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "Thực chiến dự án app mobile 2022",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black,
+          Expanded(
+            flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    "Lập trình Flutter",
+                    style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w400),
+                    textAlign: TextAlign.center,
                   ),
-                )
-              ],
-            ),
-          ))
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Thực chiến dự án app mobile 2022",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                  )
+                ],
+              ))
         ],
       ),
     );
